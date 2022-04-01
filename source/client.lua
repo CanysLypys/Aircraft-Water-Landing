fdff28894e5a517c1388989b6457aea106c56797
Collaze <77038583+Collaze@users.noreply.github.com>
2022-03-31 21:56:02 -0500

    Upload client.lua

---
+++ source/client.lua
@@ -0,0 +1,37 @@
+Citizen.CreateThread(function()
+
+    local ped = GetPlayerPed(-1)
+
+    while true do
+        Citizen.Wait(0)
+        if IsPedInAnyPlane(ped) then
+            vehicle = GetVehiclePedIsIn(ped, false)
+            if IsEntityInWater(vehicle) then
+
+                SetVehicleEngineHealth(vehicle, 0.0)
+                Wait(500)
+
+                local ModelHash = `DINGHY`
+                if not IsModelInCdimage(ModelHash) then return end
+                RequestModel(ModelHash)
+                while not HasModelLoaded(ModelHash) do
+                  Citizen.Wait(10)
+                end
+
+                floatvehicle = CreateVehicle(ModelHash, GetEntityCoords(vehicle), GetEntityHeading(ped), true, false)
+
+                --SetEntityCollision(floatvehicle, false, true)
+                SetEntityCompletelyDisableCollision(floatvehicle, false, true)
+
+                AttachEntityToEntity(vehicle, floatvehicle, 0, 0, 0, 0, 0, 0, GetEntityHeading(ped), 0, false, true, false, 0, false)
+
+                SetEntityVisible(floatvehicle, false, 0)
+                SetEntityVisible(vehicle, true, 0)
+            end
+        end
+        if not DoesEntityExist(vehicle) then
+            DeleteEntity(floatvehicle)
+        end
+    end
+
+end)
