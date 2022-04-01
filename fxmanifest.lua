1480eaf6ea02c0c6bbf7c98a9dbdef6adaf00b99
Collaze <77038583+Collaze@users.noreply.github.com>
2022-03-31 21:55:34 -0500

    Upload fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+-- Resource Metadta
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+-- Resource Information
+author 'Collaze'
+description 'Allows planes to land on water.'
+version '1.0.0'
+
+-- Scripts
+client_scripts {
+    'source/client.lua'
+}
